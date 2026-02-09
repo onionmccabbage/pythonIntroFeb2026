cd64a7466bc411347b958529d16d017ea32d61e2
onionmccabbage <onionmccabbage@live.com>
2026-02-09 12:08:35 +0000

    converting string to number

--- using_input.py
+++ using_input.py
@@ -7,9 +7,27 @@ def raisePower(n=1,m=1): # we may choose to provide default values
     return n**m
 
 # exercise the code
-a = raisePower(3,2)
-print(a) # 9
-b = raisePower() # here we let the function use its default values
-print(b) # 1
-c = raisePower(3)
-print(c) # 3
+# a = raisePower(3,2)
+# print(a) # 9
+# b = raisePower() # here we let the function use its default values
+# print(b) # 1
+# c = raisePower(3)
+# print(c) # 3
+
+
+# We may ask the user to provide an input by typing something
+g = input('Please type something: ') # the module will pause until avalue is entered
+print(f'The input is {g}')
+print(type(g)) # using input ALWAYS returns a string
+
+# how to convert a string to a number
+def makeNum(s):
+    '''where possible, convert s to a numeric value (int or float)
+    If s is NOT a numeric value, return a default of 1'''
+    if s.isnumeric():
+        num = int(float(s)) # convert to a float then to an int just to be safe
+        return num
+    else:
+        return 1
+    
+print(makeNum(g))
