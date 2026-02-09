9389445957672589c4623c47a71e6dc5bf0e7b32
onionmccabbage <onionmccabbage@live.com>
2026-02-09 10:12:58 +0000

    basic maths operators

---
+++ basics.py
@@ -0,0 +1,18 @@
+# this is a Python module
+# it contains text which will be run when we execute this module
+
+# declre variables to contain data
+a = 2   # an integer
+b = 7.3 # a floating point number
+
+print(a, b)
+print(type(a), type(b)) # we can see what type of data we have
+
+# we may carry out maths operations
+d = a+b
+e = b-a
+# we also have * /
+# % will return...
+print(b%a) # this return the remainder after division (in this case to a high precision)
+print(b//a) # this returns the whole number of divisors (modulo)
+print(2**3) # ** will raise to the power
