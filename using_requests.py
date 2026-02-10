184267b7c02dab11ca29d56dc3489c5766a61a69
onionmccabbage <onionmccabbage@live.com>
2026-02-10 10:54:37 +0000

    using exception handler

--- using_requests.py
+++ using_requests.py
@@ -5,12 +5,16 @@ import requests # we import the entire library
 def getRemoteData():
     '''retrieve a load of data about photos from a remote API'''
     # Application Programming Interface
-    api = 'http://jsonplaceholder.typicode.com/photos'
-    # we will use 'get' to retrieve all the JSON text
-    response = requests.get(api) # this will make a request to the URL
-    # we know the data is JSON in this case
-    photos = response.json() # or xml, text, html etc.
-    return photos
+    api = 'http://jjjjjsonplaceholder.typicode.com/photos'
+    # This is the sort of thing that could go wrong
+    try:
+        # we will use 'get' to retrieve all the JSON text
+        response = requests.get(api) # this will make a request to the URL
+        # we know the data is JSON in this case
+        photos = response.json() # or xml, text, html etc.
+        return photos
+    except Exception as err: # we should consider using specific exception types
+        print(f'Connection error: {err}')
 
 if __name__ == '__main__':
     # here we can exercise the code within this module
