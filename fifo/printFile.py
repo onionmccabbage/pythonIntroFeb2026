2891b09e3735e5ea0197f0263d23d6a11d55077f
onionmccabbage <onionmccabbage@live.com>
2026-02-10 15:40:32 +0000

    remember to close when no longer needed

--- fifo/printFile.py
+++ fifo/printFile.py
@@ -6,6 +6,8 @@ def printToFile(s):
     # here, if the file does not yet exist, Python will ask the OS to create it
     fout = open('my_file.txt', 'at') # 'at' will append text
     print(s, file=fout) # send vthe print output to our file access object
+    # if we have no further use for our file access object, we close it
+    fout.close() # this is good practice
 
 if __name__ == '__main__':
     str = 'here is some important text to be persisted'
