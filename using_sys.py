692a6c270b1761ddc2a1e762927eb3bedde1b05d
onionmccabbage <onionmccabbage@live.com>
2026-02-10 11:42:13 +0000

    using sys and path

--- using_sys.py
+++ using_sys.py
@@ -1,1 +1,12 @@
 # Python knows about its environment, called 'sys' for system
+
+import sys
+
+print( sys.platform )
+print( sys.version_info )
+
+# We may add places to the path sp Python will also look there when importing
+sys.path.append('D:/pythonIntroFeb2026/assets')
+
+# we can check the places that Python will look when we import stuff
+print( sys.path ) # sys.path is a list
