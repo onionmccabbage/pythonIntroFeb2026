2351069e032621d51d4322259427134b58beb9f2
onionmccabbage <onionmccabbage@live.com>
2026-02-11 10:22:53 +0000

    a sample class

--- using_classes/class_b.py
+++ using_classes/class_b.py
@@ -7,11 +7,26 @@ class Person():
     '''store name and age'''
     # we usually start with the __init__ method
     def __init__(self, n, a):
-        self.name = n
-        self.age  = a
+        # name-mangling: we put two underscores in front of data labels
+        self.__name = n # name-mangling makes it very hard to access these data members directly
+        self.__age  = a # it is nearly impossible to access these from outside the class
+    # we then declare accessor and mutator methods (getter/setter)
+    @property # the @ makes this line a decorator
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+          
+    @property
+    def age(self):
+        return self.__age
 
 if __name__ == '__main__':
     flo = Person('Floella', 43)
     Eth = Person('Ethel', 98)
     # we may access members of our class like this
-    print(flo.name, flo.age)
+    print(flo.name, flo.age) # here we call the getter-methods for name and age
+    # we may mutate data members like this
+    # Eth.age = False
+    # print(Eth.age)
